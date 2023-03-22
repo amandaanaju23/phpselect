050e1f2952a9084ed4d8a467c994b1b796e3c1ab
prof Edney <111137546+profedney@users.noreply.github.com>
2023-03-22 12:22:01 -0300

    cadastro

--- cadastro.php
+++ cadastro.php
@@ -9,6 +9,55 @@
 <body>
     <h1>Cadastro Agenda</h1>
 
+    <!-- Formulário HTML -->
+<form action="" method="post">
+  <label>Nome: </label><br>
+  <input type="text" name="nome"><br>
+  <label>Sobrenome: </label><br>
+  <input type="text" name="sobrenome"><br>
+  <label>Telefone: </label><br>
+  <input type="text" name="telefone"><br>
+  <input type="submit" value="Gravar">
+</form>
+
+<?php
+
+    
+
+
+//variaveis da conexão do banco dados
+
+$servername = "localhost";
+$username = "id19705063_escola";
+$password = "S3nh4-b4nc0d4d0s";
+$dbname = "id19705063_db1"; // nome do banco de dados que será usado
+
+// Cria a conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Coleta os dados do formulário
+  $nome = $_POST["nome"];
+  $sobrenome = $_POST["sobrenome"];
+  $telefone = $_POST["telefone"];
+
+  // Insere os dados no banco de dados
+  $sql = "INSERT INTO agenda (nome, sobrenome, telefone) VALUES ('$nome', '$sobrenome', '$telefone')";
+  if ($conn->query($sql) === TRUE) {
+    echo "Dados inseridos com sucesso"."<br>";
+  } else {
+    echo "Erro ao inserir dados: " . $conn->error;
+  }
+}
+$conn->close();
+?>
+
+
     <a href="index.php">Exibir Agenda</a>
     
 </body>
