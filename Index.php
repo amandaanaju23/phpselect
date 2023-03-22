19dae1826207890e8d3aa9b1831bc4a25b1040d6
prof Edney <111137546+profedney@users.noreply.github.com>
2023-03-22 10:57:48 -0300

    Mudou para objeto

--- Index.php
+++ Index.php
@@ -13,25 +13,26 @@ $username = "usuario";
 $password = "senha";
 $dbname = "nome_do_banco_de_dados";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-if (!$conn) {
-    die("Falha na conexão: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
 }
 
 $sql = "SELECT nome, sobrenome, telefone FROM nome_da_tabela";
-$resultado = mysqli_query($conn, $sql);
+$resultado = $conn->query($sql);
 
-if (mysqli_num_rows($resultado) > 0) {
-    while ($linha = mysqli_fetch_assoc($resultado)) {
+if ($resultado->num_rows > 0) {
+    while ($linha = $resultado->fetch_assoc()) {
         echo $linha["nome"] . " " . $linha["sobrenome"] . " - " . $linha["telefone"] . "<br>";
     }
 } else {
     echo "Nenhum registro encontrado.";
 }
 
-mysqli_close($conn);
+$conn->close();
 ?>
+
     
 </body>
 </html>
