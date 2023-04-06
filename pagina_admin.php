85ff797bd20d0696392e52f6f3f0239ec70ad342
amandaanaju23 <125404603+amandaanaju23@users.noreply.github.com>
2023-04-06 15:05:19 -0300

    ;;;;;;

---
+++ pagina_admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina_adm</title>
+</head>
+<body>
+    <h1>Agenda_admin</h1>
+<?php
+include "conexao.php";
+
+$sql = "SELECT nome, sobrenome, telefone FROM agenda";
+$resultado = $conn->query($sql);
+
+if ($resultado->num_rows > 0) {
+    while ($linha = $resultado->fetch_assoc()) {
+        echo $linha["nome"] . " " . $linha["sobrenome"] . " - " . $linha["telefone"] . "<br>";
+    }
+} else {
+    echo "Nenhum registro encontrado.";
+}
+
+$conn->close();
+?>
+
+<a href="cadastro.php">Inserir novo cadastro</a>
+
+    
+</body>
+</html>
+
+
